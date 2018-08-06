3e5bb0ea1db9adaceb54a05e8698dd32503ff1aa
vvvcode <857783235@qq.com>
2018-08-06 19:01:51 +0800

    add pom,test ssh

---
+++ src/main/java/common/EmailUtil.java
@@ -0,0 +1,12 @@
+/**
+ * FileName: EmailUtil
+ * Author:   Administrator
+ * CreatedDate:     2018/8/6 18:21
+ * UpdatedDate:     2018/8/6 18:21
+ * Description: 邮件工具类
+ */
+
+package common;
+
+public class EmailUtil {
+}
